5f2098bcae5acb906b48561455548d986133f9bc
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-05-26 02:02:55 +0300

    Fix ESLint errors

--- frontend/src/components/code/Code.tsx
+++ frontend/src/components/code/Code.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Code(data: any) {
     return (
         <pre className="code">{JSON.stringify(data, null, 2)}</pre>
