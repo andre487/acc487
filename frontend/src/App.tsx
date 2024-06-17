654f736629cb85b835e72dd8cb4c905025dda7f0
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2024-06-17 19:06:51 +0200

    Remove @silevis/reactgrid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import AccDataHandler from '@components/accDataHandler/AccDataHandler.tsx';
 import ErrorListener from '@components/errorListener/ErrorListener.tsx';
 import Grid from '@components/grid/Grid.tsx';
-import Grid2 from '@components/grid2/Grid.tsx';
 import Header from '@components/header/Header.tsx';
 import NotificationViewer from '@components/notificationViewer/NotificationViewer.tsx';
 import {useAppContext} from '@context/appContext.ts';
@@ -28,7 +27,6 @@ export default function App() {
                 setErrors={setErrors}
                 notifications={notifications}
                 setNotifications={setNotifications} />
-            <Grid2 accId={0} accPureData={accPureData} setAccPureData={setAccPureData} />
             <Grid accId={0} accPureData={accPureData} setAccPureData={setAccPureData} />
         </>
     );
--- frontend/src/components/grid/Grid.tsx
+++ frontend/src/components/grid/Grid.tsx
@@ -1,9 +1,14 @@
-import {ReactGrid, CellChange, DefaultCellTypes} from '@silevis/reactgrid';
-import '@silevis/reactgrid/styles.scss';
-import './Grid.scss';
+import {AccPureDataSetter} from '@context/appContext.ts';
+import {HotTable, HotTableClass} from '@handsontable/react';
 import {AccountStateData} from '@models/acc.ts';
 import {AccountStatePureData} from '@typings/acc.ts';
-import {AccPureDataSetter} from '@context/appContext.ts';
+import Handsontable from 'handsontable';
+import {registerAllModules} from 'handsontable/registry';
+import {useRef} from 'react';
+import {hyperFormulaInstance} from './formulas.ts';
+import './Grid.scss';
+
+registerAllModules();
 
 export interface GridProps {
     accId: number;
@@ -13,24 +18,40 @@ export interface GridProps {
 
 export default function Grid({accId, accPureData, setAccPureData}: GridProps) {
     const accData = new AccountStateData(accPureData.accounts);
+    const hotRef = useRef<HotTableClass>(null);
 
-    const handleChanges = (changes: CellChange<DefaultCellTypes>[]) => {
-        const changed = accData.applyChanges(accId, changes);
+    function onAfterChange(change: Handsontable.CellChange[] | null, source: Handsontable.ChangeSource) {
+        if (!change || source !== 'edit') {
+            return;
+        }
+        const data = hotRef.current?.hotInstance?.getData();
+        const changed = accData.applyHotTableChanges(accId, data);
         setAccPureData({
             ...accData,
             accounts: changed.accounts,
             version: changed.version,
         });
-    };
+    }
 
     return (
-        <div className="accGrid">
-            <ReactGrid
-                rows={accData.getRows(accId)}
-                columns={accData.getColumns()}
-                onCellsChanged={handleChanges}
-                enableRangeSelection={true}
-                enableColumnSelection={true} />
-        </div>
+        <HotTable
+            ref={hotRef}
+            data={accData.getHotTableData(accId)}
+            columns={accData.getHotTableColumnSettings()}
+            cell={accData.getHotTableCellSettings()}
+            rowHeaders={true}
+            colHeaders={true}
+            height="auto"
+            width="auto"
+            minSpareRows={1}
+            autoWrapRow={true}
+            autoWrapCol={true}
+            licenseKey="non-commercial-and-evaluation"
+            formulas={{
+                engine: hyperFormulaInstance,
+                sheetName: 'Sheet1',
+            }}
+            afterChange={onAfterChange}
+        />
     );
 }
